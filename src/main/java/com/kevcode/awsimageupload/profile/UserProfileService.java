619ded4b78d190daa125d6221382242bb7758b0a
Kevin Staniszewski <kevingstanisz@gmail.com>
2020-09-14 20:53:10 -0500

    can upload images to S3 - 1:53:43

--- src/main/java/com/kevcode/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/kevcode/awsimageupload/profile/UserProfileService.java
@@ -1,20 +1,77 @@
 package com.kevcode.awsimageupload.profile;
 
+import com.kevcode.awsimageupload.bucket.BucketName;
+import com.kevcode.awsimageupload.filestore.FileStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.http.entity.ContentType.*;
 
 @Service
 public class UserProfileService {
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
     @Autowired
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles() {
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. check if image is not active
+        isFileEmpty(file);
+
+        // 2. if file is an image
+        isImage(file);
+
+        // 3. the user exists in the database
+        UserProfile user = getUserProfileOrThrow(userProfileId);
+
+        // 4. grab some metadata from file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+
+        // 5. store the image in s3 and update database with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String filename = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
+
+
+        try {
+            fileStore.save(path, filename, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessService
+                .getUserProfiles()
+                .stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+    }
+
+    private void isImage(MultipartFile file) {
+        if(Arrays.asList(IMAGE_JPEG, IMAGE_PNG, IMAGE_GIF).contains(file.getContentType())) {
+            throw new IllegalStateException("file must be an image");
+        }
+    }
+
+    private void isFileEmpty(MultipartFile file) {
+        if(file.isEmpty()) {
+            throw new IllegalStateException("cannot upload empty file [ " + file.getSize() + "]");
+        }
     }
 }
